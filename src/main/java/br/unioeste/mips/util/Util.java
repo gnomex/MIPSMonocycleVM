48c9d892ad4be5a0abe9ca0356a629eda4c938a6
Kenner Kliemann <kenner.hp@gmail.com>
2013-11-01 21:01:46 -0200

    Updating OPCODES

--- src/main/java/br/unioeste/mips/util/Util.java
+++ src/main/java/br/unioeste/mips/util/Util.java
@@ -32,17 +32,17 @@ public final class Util {
 	public static final int SUB	=	0x00000022; //100010 - 34
 	public static final int AND	=	0x00000024; //100100 - 36
 	public static final int OR	=	0x00000025; //100101 - 37
-	public static final int ADDi = 0x00000001;
-	public static final int ANDi = 0x00000002;
-	public static final int ORi = 0x00000003;
-	public static final int LUI = 0x00000004;
-	public static final int LW = 0x00000005;
-	public static final int SW = 0x00000006;
-	public static final int BEQ = 0x00000007;
-	public static final int BNE = 0x00000008;
-	public static final int j = 0x00000009;
-	public static final int jr = 0x0000000A;
-	public static final int LDI = 0x0000000B;
+	public static final int ADDi = 0x00000008;
+	public static final int ANDi = 0x0000000C;
+	public static final int ORi = 0x0000000D;
+	public static final int LUI = 0x0000000F;
+	public static final int LW = 0x00000023;
+	public static final int SW = 0x0000002B;
+	public static final int BEQ = 0x00000004;
+	public static final int BNE = 0x00000005;
+	public static final int j = 0x00000002;
+	public static final int jr = 0x00000000;
+	public static final int LDI = 0x00000001;
 	
 	/*
 	 * Loggers
